e5af4eb2c627fd1d6fa9fb6173c12a50a067d444
Jeremy Castagno <JeremyBYU@gmail.com>
2015-05-10 21:54:21 -0500

    Flip Coil CheckBoxes
    Added Flip Coild Checkboxes to demonstrate.

--- client/views/ace/ace.js
+++ client/views/ace/ace.js
@@ -1,1 +1,28 @@
 x = 1;
+Template.ace.rendered = function() {
+    $('.ui.checkbox')
+        .checkbox();
+}
+
+Template.ace.events({
+
+    // 'click .log-out': function () {
+    //   Meteor.logout();
+    // }
+    "change .ui.checkbox input": function(event) {
+
+        console.log("Attempting to toggle " + event.target.name);
+        states = [true];
+        if (event.target.checked != true) {
+            states = [false];
+        }
+        if (event.target.name == 'coil1') {
+            Meteor.call('writeCoils', 0x0000, states);
+
+        } else {
+
+            Meteor.call('writeCoils', 0x0001, states);
+        }
+        //Session.set("hideCompleted", event.target.checked);
+    }
+})
